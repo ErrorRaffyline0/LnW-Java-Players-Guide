9bbd301aa695d35e30acd57546ef4fd10f25030e
Raffy <rafaelroos100@gmail.com>
2024-09-20 15:44:25 +0200

    renamed files to match order, removed redundant files, adds ConsolasAndTelim

---
+++ exercises/practice/002_WhatComesNext.java
@@ -0,0 +1,7 @@
+public class 002_WhatComesNext
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Rafael!");
+    }
+}
---
+++ exercises/practice/004_ConsolasAndTelim.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 004_ConsolasAndTelim
+{
+    public static void main(String[] args)
+    {
+        Scanner Bakery = new Scanner(System.in);
+        System.out.println("Bread is ready.");
+        System.out.println("Who is the bread for?");
+
+        String Name = Bakery.nextLine();
+        System.out.println("Noted: " + Name + " got bread.");
+    }
+}
