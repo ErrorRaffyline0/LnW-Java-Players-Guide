90b31acab52a106115e65c9d2fd62afec81949f6
Raffy <rafaelroos100@gmail.com>
2024-11-06 14:28:18 +0100

    adds C21, fixes non-updated gitignore and removes redundant folder naming

---
+++ src/C21_Countdown.java
@@ -0,0 +1,13 @@
+public class C21_Countdown {
+    public static void main(String[] args) {
+        int result = min(10);
+        System.out.println(result);
+    }
+
+    public static int min(int k) {
+        if (k > 1) {
+            return min(k - 1);
+        } else
+            return 1;
+    }
+}
